7450de048db69ab3d9eb3fb8f0c8a71601a353a9
Avi Bhandari <86038001+hcgjhb@users.noreply.github.com>
2022-11-02 22:47:48 +0530

    Add files via upload

---
+++ Hangman_guessing_game/main.py
@@ -0,0 +1,54 @@
+
+
+import random
+import hangman_art
+import hangman_words
+
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+print(hangman_art.logo)
+
+
+
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    # If the user has entered a letter they've already guessed, print the letter and let them know.
+    if guess in display:
+      print("You have already guessed this letter")
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #Check if user is wrong.
+    if guess not in chosen_word:
+      print(f"{guess} is not the correct letter. Try again")
+        # If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+      lives -= 1
+      if lives == 0:
+        end_of_game = True
+        print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
+    print(hangman_art.stages[lives])
+print(f"The correct letter is {chosen_word}")
